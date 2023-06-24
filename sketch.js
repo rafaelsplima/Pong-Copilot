f2cdc0c468132f61b3dd7bd4823f421819a21a80
Rafael Lima <rafaelsplima@gmail.com>
2023-06-24 11:21:20 -0300

    chore: refatoracoes usando copilot labs

--- sketch.js
+++ sketch.js
@@ -1,33 +1,42 @@
-// posicao da bola
-let x = 200;
-let y = 200;
+class Bola {
+    constructor() {
+        this.r = 25;
+        this.reset();
+    }
 
-// velocidade da bola aleatoria
-let vx = Math.random() * 10 - 5;
-let vy = Math.random() * 10 - 5;
+    reset() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.vx = Math.random() * 10 - 5;
+        this.vy = Math.random() * 10 - 5;
+    }
 
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.x < this.r || this.x > width - this.r) {
+            this.reset();
+        }
+        if (this.y < this.r || this.y > height - this.r) {
+            this.vy *= -1;
+        }
+    }
 
-
-
-// funcao setup do p5.js
-function setup() {
-    createCanvas(400, 400);
+    desenha() {
+        fill(color(255, 0, 0))
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
 }
 
-// funcao de desenho do p5.js
+let bola;
+
+function setup() {
+    createCanvas(800, 400);
+    bola = new Bola();
+}
+
 function draw() {
-    background(0);
-    fill(255);
-    ellipse(x, y, 50, 50);
-    x+=vx;
-    y+=vy;
-
-    // se tocar na parede, inverte a velocidade
-    if (x > width || x < 0) {
-        vx *= -1;
-    }
-    if (y > height || y < 0) {
-        vy *= -1;
-    }
-
+    background(color(0, 0, 0));
+    bola.update();
+    bola.desenha();
 }
